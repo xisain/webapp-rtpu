054a9870493e8d2f7db703b2db496e958107c030
xisain <husenabs232bgr@gmail.com>
2025-08-27 19:53:59 +0700

    first commit

---
+++ app/Http/Controllers/ProdukUnggulanController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\produk_unggulan;
+use Illuminate\Http\Request;
+
+class ProdukUnggulanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(produk_unggulan $produk_unggulan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(produk_unggulan $produk_unggulan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, produk_unggulan $produk_unggulan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(produk_unggulan $produk_unggulan)
+    {
+        //
+    }
+}
