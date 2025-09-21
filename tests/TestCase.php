87dd4b39df409887507534880a1d72f3d30a60cf
xisain <husenabs232bgr@gmail.com>
2025-09-22 04:53:44 +0700

    testing pt 2

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,13 +6,5 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    protected function setUp(): void
-{
-    parent::setUp();
-
-    // Jalankan migration
-    $this->artisan('migrate');
-    $this->seed(\Database\Seeders\RoleSeeder::class);
-
-}
+  
 }
